9e0b68f7876300f5ca0cdeb539edde43ec4dbf1e
guilleves <guilleves@gmail.com>
2015-08-23 16:54:32 -0300

    added show action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,13 +1,21 @@
 class MessagesController < ApplicationController
 
+def show
+	@message = Message.find(params[:id])
+	#returns object corresponding to the given id -- id is the parameter needed by the router
+end
+
 def new
 end
 
 def create
-	@message = Message.new(params[:message])
-
+	@message = Message.new(message_params)
 	@message.save
 	redirect_to @message
 end
 
+private 
+	def message_params
+		params.require(:message).permit(:from, :text)
+	end
 end
