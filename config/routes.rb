49762a5b490fa0b7e0f35267375fec4f8060e9d6
guilleves <guilleves@gmail.com>
2015-08-23 23:57:58 -0300

    names are finally showing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   #get 'welcome/index'
 	get 'messages/new'
-  get 'messages#show'
   resources :messages
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
